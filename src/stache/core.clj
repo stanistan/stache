1159cb53357d83243d067b843f2b3f38b47115c9
Stan Rozenraukh <stan@stanistan.com>
2012-08-30 21:23:41 -0400

    better arg destructuring, rename some vars

--- src/stache/core.clj
+++ src/stache/core.clj
@@ -5,26 +5,26 @@
 ;; Constants..............................................................................
 
 (def config-default
+  "Default configuration options for what we're looking for, and where"
   {:path-to "templates"
    :suffix ".mustache"})
 
-(def partial-pattern #"\{\{\>([\w\/-]+)\}\}") ; the pattern used to define partials
+(def partial-pattern
+  "The pattern used to define partials"
+  #"\{\{\>([\w\/-]+)\}\}")
 
 ;; Settings and Configs...................................................................
 
-(declare path-to) ; the resource path where mustache templates are
-(declare suffix)  ; suffix used for the files
+(declare path-to-templates) ; the resource path where mustache templates are
+(declare template-suffix)  ; suffix used for the files
 
 (defn config
   "Bind values to path-to or suffix.
    Usage: (config :path-to \"path/to/mustaches/\" :suffix \".m\")"
-  [& e]
-  (let [e (apply hash-map e)
-        p (:path-to e)
-        s (:suffix e)]
-    (do
-      (and p (def path-to p))
-      (and s (def suffix s)))))
+  [& {:keys [path-to suffix]}]
+  (do
+    (and path-to (def path-to-templates path-to))
+    (and suffix (def template-suffix suffix))))
 
 (defn reset-config
   "Resets the settings to defaults."
@@ -36,10 +36,12 @@
 
 ;; Helpers................................................................................
 
-(defn not-nil? [n]
+(defn not-nil?
+  [n]
   (not (nil? n)))
 
-(defn has-match [rgx s]
+(defn has-match
+  [rgx s]
   (not-nil? (re-find rgx s)))
 
 ;; Directory Utitlies.....................................................................
@@ -47,7 +49,7 @@
 (defn mustache-path
   "Gets a resource path to the mustache file"
   [m]
-  (str path-to "/" m suffix))
+  (str path-to-templates "/" m template-suffix))
 
 (defn get-template
   "Gets the contents of the mustache file"
@@ -88,8 +90,7 @@
   "Render the given mustache template with the data given.
    Finds partials automatically unless they are specified using:
    (render-template m data :partials {:name partial})"
-  [m data & e]
-  (let [e (apply hash-map e)
-        m (if (mustache? m) m (get-template m))
-        partials (get-partials m (or (:partials e) {}))]
+  [m data & {:keys [partials]}]
+  (let [m (if (mustache? m) m (get-template m))
+        partials (get-partials m (or partials {}))]
     (parser/render m data partials)))
--- test/stache/core_test.clj
+++ test/stache/core_test.clj
@@ -18,18 +18,18 @@
 (deftest configs
 
   (testing "Default configurations"
-    (is (= (:path-to config-default) path-to))
-    (is (= (:suffix config-default) suffix)))
+    (is (= (:path-to config-default) path-to-templates))
+    (is (= (:suffix config-default) template-suffix)))
 
   (config :path-to "resources" :suffix ".m")
   (testing "Changing config"
-    (is (= path-to "resources"))
-    (is (= suffix ".m")))
+    (is (= path-to-templates "resources"))
+    (is (= template-suffix ".m")))
 
   (reset-config)
   (testing "Rest to default configurations"
-    (is (= (:path-to config-default) path-to))
-    (is (= (:suffix config-default) suffix))))
+    (is (= (:path-to config-default) path-to-templates))
+    (is (= (:suffix config-default) template-suffix))))
 
 (deftest resources
 

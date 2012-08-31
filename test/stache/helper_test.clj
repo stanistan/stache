3363e74d4c85ef5e00304232f2cc99912a5ac5c9
Stan Rozenraukh <stan@stanistan.com>
2012-08-31 10:01:28 -0400

    more test for helper

--- test/stache/helper_test.clj
+++ test/stache/helper_test.clj
@@ -23,9 +23,13 @@
       "some/path/to/place/continued/too"))))
 
 (deftest others
+
   (testing "not-nil"
     (is (= (not-nil? nil) false))
     (is (= (not-nil? 'a) true))
     (is (= (not-nil? "a") true))
-    (is (= (not-nil? 1)))
-    ))
+    (is (= (not-nil? 1))))
+
+  (testing "has-match"
+    (is (= (has-match #"a" "a") true))
+    (is (= (has-match #"[a-z]+" "123") false))))

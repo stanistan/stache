8dd2ed4281b238628aa30a6d43b6e82ee9ea91f5
Stan Rozenraukh <stan@stanistan.com>
2012-09-04 15:34:52 -0400

    refacotr config

--- src/stache/config.clj
+++ src/stache/config.clj
@@ -11,13 +11,26 @@
 ; suffix used for the files
 (declare template-suffix)
 
+(defn- current-configuration
+  []
+  {:path-to path-to-templates
+   :suffix template-suffix})
+
+(defn or-defaults
+  [& {:keys [path-to suffix]
+      :or {path-to (:path-to current-configuration)
+           suffix (:suffix current-configuration)}}]
+  [path-to suffix])
+
 (defn config
   "Bind values to path-to or suffix.
    Usage: (config :path-to \"path/to/mustaches/\" :suffix \".m\")"
-  [& {:keys [path-to suffix]}]
-  (do
-    (and path-to (def path-to-templates path-to))
-    (and suffix (def template-suffix suffix))))
+  [& args]
+  (let [[p s] (apply or-defaults args)]
+    (do
+      (def path-to-templates p)
+      (def template-suffix s)
+      (current-configuration))))
 
 (defn reset-config
   "Resets the settings to defaults."
